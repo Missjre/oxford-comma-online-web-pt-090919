714ec5932ea65130108f82c691cfe6b7fba062e9
Jalysa Reason <jalysareason@gmail.com>
2019-09-22 10:55:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,5 +4,13 @@ def # call join on the array
   puts results
 end
 
-def 
-
+def oxford_comma
+    oxford_comma(["kiwi", "durian"]=("kiwi and durian")
+  end
+  
+  it "adds commas plus a final 'and' when given a 3-element array" do
+    expect(oxford_comma(["kiwi", "durian", "starfruit"])).to eq("kiwi, durian, and starfruit")
+  end
+  it "correctly formats arrays of lengths greater than three" do
+    expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])).to eq("kiwi, durian, starfruit, mangos, and dragon fruits")
+    expect(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])).to eq("kiwi, du
